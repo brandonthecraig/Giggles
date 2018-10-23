6c726e3b199b5e7d8b2bb06b4eb6c77ce855c357
Brandon Craig <BrandonCraig@rentalcars.com>
2018-10-23 16:53:51 +0100

    Created a New Project
    New project

---
+++ src/main/java/com/brandongo/giggles/GigglesApplication.java
@@ -0,0 +1,12 @@
+package com.brandongo.giggles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GigglesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GigglesApplication.class, args);
+    }
+}
---
+++ src/test/java/com/brandongo/giggles/GigglesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.brandongo.giggles;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GigglesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
